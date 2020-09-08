6f4a480147227c1e73f5b7831648d3a055982b9b
puli <123@123.com>
2020-09-08 23:21:30 +0800

    双数据源

--- src/main/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplication.java
+++ src/main/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplication.java
@@ -2,8 +2,13 @@ package com.gupao.example.springbootstarterdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
 public class SpringbootStarterDemoApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplicationTests.java
+++ src/test/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplicationTests.java
@@ -7,21 +7,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootStarterDemoApplicationTests {
 
     @Test
     public void contextLoads() {
+//        System.out.println(dataSource.getClass());
     }
 
 
     @Autowired
+    JdbcTemplate db1JdbcTemplate;
+
+    @Resource(name = "db2JdbcTemplate")
     JdbcTemplate jdbcTemplate;
+//    @Autowired
+//    DataSource dataSource;
 
     @Test
     public void addData(){
         String sql = "insert into user_info(name,age) values('pupu',18)";
+
+
+        db1JdbcTemplate.execute(sql);
+    }
+
+    @Test
+    public void addData2(){
+        String sql = "insert into user_info(name,age) values('pupu2',18)";
 
 
         jdbcTemplate.execute(sql);
