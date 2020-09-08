da9eff21f8a201e2a3d632980d1b78b9e388f369
puli <123@123.com>
2020-09-08 22:39:49 +0800

    jdbcTemplate

--- src/test/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplicationTests.java
+++ src/test/java/com/gupao/example/springbootstarterdemo/SpringbootStarterDemoApplicationTests.java
@@ -2,7 +2,9 @@ package com.gupao.example.springbootstarterdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,18 @@ public class SpringbootStarterDemoApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void addData(){
+        String sql = "insert into user_info(name,age) values('pupu',18)";
+
+
+        jdbcTemplate.execute(sql);
     }
 
 }
